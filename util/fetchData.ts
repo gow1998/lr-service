230f1b95bb19338dcfb8c5a7c2a5e805475d600e
gow1998 <115560712+gow1998@users.noreply.github.com>
2023-04-04 17:01:32 +0530

    Add files via upload

---
+++ util/fetchData.ts
@@ -0,0 +1,55 @@
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom, map } from 'rxjs';
+import * as qs from 'qs';
+
+export const fetchDataFromAcessTokenPost = async (
+  access_token: string,
+  info_uri: string,
+  httpService: HttpService,
+) => {
+  const res = await lastValueFrom(
+    httpService
+      .post(
+        info_uri,
+        {},
+        { headers: { Authorization: `Bearer ${access_token}` } },
+      )
+      .pipe(map((item) => item.data)),
+  );
+  return res;
+};
+
+export const fetchDataFromAcessTokenGet = async (
+  access_token: string,
+  info_uri: string,
+  httpService: HttpService,
+) => {
+  const res = await lastValueFrom(
+    httpService
+      .get(info_uri, { headers: { Authorization: `Bearer ${access_token}` } })
+      .pipe(map((item) => item.data)),
+  );
+  return res;
+};
+
+export const getAccessTokenFromCreds = async (
+  data: any,
+  authURI: string,
+  httpService: HttpService,
+  clientIDSecret: string,
+) => {
+  const config: any = {
+    url: authURI,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${clientIDSecret}`,
+    },
+    data: data,
+  };
+  const res = await lastValueFrom(
+    httpService
+      .post(config.url, config.data, { headers: config.headers })
+      .pipe(map((item) => item.data)),
+  );
+  return res;
+};
